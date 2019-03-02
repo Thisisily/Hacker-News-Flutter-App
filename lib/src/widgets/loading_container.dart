6ff188493872b8f42853a86be1f4a876f99fe153
Thisisily <44399258+Thisisily@users.noreply.github.com>
2019-03-02 15:15:31 +0330

    Add files via upload

---
+++ lib/src/widgets/loading_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoadingContainer extends StatelessWidget {
+  final Widget child;
+
+  LoadingContainer({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title:buildContainer() ,
+          subtitle: buildContainer(),
+        ),
+        Divider(height: 8.0,)
+      ],
+    );
+  }
+  Widget buildContainer (){
+    return Container(
+      color: Colors.grey[200],
+      height: 24.0,
+      width: 150.0,
+      margin: EdgeInsets.only(top: 5.0,bottom: 5.0),
+    );
+  }
+}
